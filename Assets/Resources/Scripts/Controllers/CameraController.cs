a99c4a622c18f53b11c830d6fce1817ea0d4fa49
thefishlive <thefishlive@hotmail.com>
2016-12-10 10:33:02 +0000

    Add basic camera control

--- Assets/Resources/Scripts/Controllers/CameraController.cs
+++ Assets/Resources/Scripts/Controllers/CameraController.cs
@@ -1,16 +1,49 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class CameraController : MonoBehaviour {
+public class CameraController : MonoBehaviour
+{
+    public Vector3 MovementSpeed = new Vector3(1, 1, 1);
+    public Vector2 LookSpeed = new Vector2(1, 1);
+
+    public float LookMinY = -80.0f;
+    public float LookMaxY = 80.0f;
+
+    public Camera Camera;
+
+    private CameraControls m_controls;
+
 
 	// Use this for initialization
-	void Start () {
-		
-	}
+	void Start ()
+    {
+		m_controls = new CameraControls();
+    }
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+	void FixedUpdate ()
+	{
+	    var movement = new Vector3();
+
+	    movement += MovementSpeed.x * m_controls.MoveXZ.X * Camera.transform.right;
+	    movement += MovementSpeed.y * m_controls.MoveY.Value * Camera.transform.up;
+	    movement += MovementSpeed.z * m_controls.MoveXZ.Y * Camera.transform.forward;
+        
+	    transform.position += movement;
+
+        transform.Rotate(transform.up, LookSpeed.x * m_controls.Look.X);
+
+        var vertAngle = Camera.transform.eulerAngles.x;
+        vertAngle += m_controls.Look.Y * LookSpeed.y;
+
+        if (vertAngle <= 180.0f && vertAngle > -LookMinY)
+        {
+            vertAngle = -LookMinY;
+        }
+
+        if (vertAngle >= 180.0f && vertAngle < (360 - LookMaxY))
+        {
+            vertAngle = 360 - LookMaxY;
+        }
+
+        Camera.transform.rotation = Quaternion.Euler(new Vector3(vertAngle, Camera.transform.eulerAngles.y, Camera.transform.eulerAngles.z));
+    }
 }
--- Assets/Resources/Scripts/Controllers/Controls/CameraControls.cs
+++ Assets/Resources/Scripts/Controllers/Controls/CameraControls.cs
@@ -1,16 +1,71 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using InControl;
 using UnityEngine;
 
-public class CameraControls : MonoBehaviour {
+public class CameraControls : PlayerActionSet
+{
+    public PlayerAction MoveLeft;
+    public PlayerAction MoveRight;
+    public PlayerAction MoveForward;
+    public PlayerAction MoveBackwards;
+    public PlayerAction MoveUp;
+    public PlayerAction MoveDown;
+    public PlayerTwoAxisAction MoveXZ;
+    public PlayerOneAxisAction MoveY;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public PlayerAction LookLeft;
+    public PlayerAction LookRight;
+    public PlayerAction LookUp;
+    public PlayerAction LookDown;
+    public PlayerTwoAxisAction Look;
+
+    public PlayerAction ZoomIn;
+    public PlayerAction ZoomOut;
+    public PlayerOneAxisAction Zoom;
+    
+    public CameraControls()
+    {
+        MoveLeft = CreatePlayerAction("input.move.left");
+        MoveRight = CreatePlayerAction("input.move.right");
+        MoveForward = CreatePlayerAction("input.move.forward");
+        MoveBackwards = CreatePlayerAction("input.move.backwards");
+        MoveUp = CreatePlayerAction("input.move.up");
+        MoveDown = CreatePlayerAction("input.move.down");
+
+        MoveXZ = CreateTwoAxisPlayerAction(MoveLeft, MoveRight, MoveBackwards, MoveForward);
+        MoveY = CreateOneAxisPlayerAction(MoveUp, MoveDown);
+
+        LookLeft = CreatePlayerAction("input.look.left");
+        LookRight = CreatePlayerAction("input.look.right");
+        LookUp = CreatePlayerAction("input.look.up");
+        LookDown = CreatePlayerAction("input.look.down");
+
+        Look = CreateTwoAxisPlayerAction(LookLeft, LookRight, LookDown, LookUp);
+
+        ZoomIn = CreatePlayerAction("input.zoom.in");
+        ZoomOut = CreatePlayerAction("input.zoom.out");
+
+        Zoom = CreateOneAxisPlayerAction(ZoomOut, ZoomIn);
+
+        SetupDefaultKeyboard();
+    }
+
+    public void SetupDefaultKeyboard()
+    {
+        MoveLeft.AddDefaultBinding(Key.A);
+        MoveRight.AddDefaultBinding(Key.D);
+        MoveForward.AddDefaultBinding(Key.W);
+        MoveBackwards.AddDefaultBinding(Key.S);
+        MoveUp.AddDefaultBinding(Key.Shift);
+        MoveDown.AddDefaultBinding(Key.Space);
+
+        LookLeft.AddDefaultBinding(Mouse.NegativeX);
+        LookRight.AddDefaultBinding(Mouse.PositiveX);
+        LookUp.AddDefaultBinding(Mouse.NegativeY);
+        LookDown.AddDefaultBinding(Mouse.PositiveY);
+
+        ZoomIn.AddDefaultBinding(Mouse.NegativeScrollWheel);
+        ZoomOut.AddDefaultBinding(Mouse.PositiveScrollWheel);
+    }
 }
